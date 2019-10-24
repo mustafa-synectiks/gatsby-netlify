1a61be297d5c1818b689b509ded6b1cd66c26c0a
mustafa-synectiks <35029729+mustafa-synectiks@users.noreply.github.com>
2019-10-24 17:38:01 +0530

    Added Image

--- src/pages/CaseStudies/Storagemigration.js
+++ src/pages/CaseStudies/Storagemigration.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Helmet } from "react-helmet"
-import cb from "../../images/Storagemigration.jpg"
+import cb from "../../images/StorageMigration.jpg"
 import as from "../../images/Application-Services-Slider.png"
 import Layout from "../../components/layout"
 
